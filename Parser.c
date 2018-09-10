d4018ac19f9573aecb0116c34822439d44fea0c6
guywaldman <guywaldman@guys-mbp>
2018-09-10 09:59:47 +0300

    shortened parser

--- Parser.c
+++ Parser.c
@@ -30,10 +30,15 @@
 #define EXIT "exit"
 #define ERROR_INV_CMD "ERROR: invalid command\n"
 
-int checkToken(char * cmd){
-	if(cmd == NULL) {
-		printf(ERROR_INV_CMD); /*invalid command*/
-		return 0;
+int checkTokens(char * cmd, char ** cmdArr, int inputs){
+	int i;
+	for (i = 0; i < inputs; i++) {
+		cmd = strtok(NULL, DELIMITER);
+		if(cmd == NULL) {
+			printf(ERROR_INV_CMD); /*invalid command*/
+			return 0;
+		}
+		cmdArr[i] = cmd;
 	}
 	return 1;
 }
@@ -51,8 +56,7 @@ int getCommand(gameData * game, char ** cmdArr) {
 	}
 	while (cmd == NULL); /*only whitespace*/
 	if (strcmp(cmd, SOLVE) == 0) {
-		cmd = strtok(NULL, DELIMITER);
-		if(checkToken(cmd) == 0) { return 0; } /*invalid command*/
+		if (checkTokens(cmd, cmdArr, 1) == 0) { return 0; }
 		solve(game, cmd);
 	}
 	else if (strcmp(cmd, EDIT) == 0) {
@@ -60,50 +64,27 @@ int getCommand(gameData * game, char ** cmdArr) {
 		edit(game, cmd);
 	}
 	else if (strcmp(cmd, MARK) == 0) {
-		cmd = strtok(NULL, DELIMITER);
-		if(checkToken(cmd) == 0) { return 0; }
-		cmdArr[0] = cmd;
+		if (checkTokens(cmd, cmdArr, 1) == 0) { return 0; }
 		markErrors(game, cmdArr);
 	}
-	else if (strcmp(cmd, PRINT) == 0) {
-		printBoard(game);
-	}
+	else if (strcmp(cmd, PRINT) == 0) {	printBoard(game); }
 	else if (strcmp(cmd, SET) == 0) {
-		for (i = 0; i < 3; i++) {
-			cmd = strtok(NULL, DELIMITER);
-			if(checkToken(cmd) == 0) { return 0; }
-			cmdArr[i] = cmd;
-		}
+		if (checkTokens(cmd, cmdArr, 3) == 0) { return 0; }
 		set(game, cmdArr);
 	}
 	else if (strcmp(cmd, VALIDATE) == 0) { validate(game, 1); }
 	else if (strcmp(cmd, GENERATE) == 0) {
-		for (i = 0; i < 2; i++) {
-			cmd = strtok(NULL, DELIMITER);
-			if(checkToken(cmd) == 0) {
-				return 0;
-			}
-			cmdArr[i] = cmd;
-		}
+		if (checkTokens(cmd, cmdArr, 2) == 0) { return 0; }
 		generate(game, cmdArr);
 	}
 	else if (strcmp(cmd, UNDO) == 0) { undo(game, 1); }
 	else if (strcmp(cmd, REDO) == 0) { redo(game); }
 	else if (strcmp(cmd, SAVE) == 0) {
-		cmd = strtok(NULL, DELIMITER);
-		if(checkToken(cmd) == 0) {
-			return 0;
-		}
+		if (checkTokens(cmd, cmdArr, 1) == 0) { return 0; }
 		save(game, cmd);
 	}
 	else if (strcmp(cmd, HINT) == 0) {
-		for (i = 0; i < 2; i++) {
-			cmd = strtok(NULL, DELIMITER);
-			if(checkToken(cmd) == 0) {
-				return 0;
-			}
-			cmdArr[i] = cmd;
-		}
+		if (checkTokens(cmd, cmdArr, 2) == 0) { return 0; }
 		hint(game, cmdArr);
 	}
 	else if (strcmp(cmd, SOLS) == 0) { numSols(game); }
