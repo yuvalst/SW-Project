98de5d04288c8b39f069e02f3582202c5a6e7ad1
Guy <39929045+guywal@users.noreply.github.com>
2018-09-16 18:06:31 +0300

    Update Stack.c

--- Stack.c
+++ Stack.c
@@ -12,43 +12,41 @@
 #include "Stack.h"
 #include "GameAux.h"
 
-/* Receives gameData and stack pointer and inisializes a new stack object*/
+
 void initStack(gameData * game, stack *stk)
 {
-   stk->top = EMPTY;
+   stk->top = EMPTY; /* -1 */
    stk->full = game->bSize * game->bSize - 1;
    stk->arr = (int*)calloc(3*(stk->full + 2),sizeof(int));
    checkAlloc(stk->arr);
 }
 
-/* Receives stack, i and j for coordinates on the board and z for value and pushes them to the stack array */
 void push(stack *stk, int i, int j, int z) {
-    stk->top++;
+    stk->top++; /*increment index*/
     stk->arr[stk->top*3] = i;
     stk->arr[stk->top*3+1] = j;
     stk->arr[stk->top*3+2] = z;
 }
 
 /* Receives stack and int pointer and calls top to update cell, 
-but also removes the last cell from the stack by decrising top*/
+but also removes the last cell from the stack by decreasing top*/
 void pop(stack *stk, int* cell) {
   top(stk, cell);
   stk->top--;
 }
 
-/* Receives stack and int pointer and updates cell with the values of the last cell on the stack like top*/
 void top(stack *stk, int* cell) {
 	cell[0] = stk->arr[stk->top*3];
 	cell[1] = stk->arr[stk->top*3+1];
 	cell[2] = stk->arr[stk->top*3+2];
 }
 
-/* Receives stack and returns 1 if the stack is empty else 0*/
+
 int isEmpty(stack *stk) {
 	return ((stk->top == EMPTY));
 }
 
-/* Receives stack and returns 1 if the stack is full else 0*/
+
 int isFull(stack *stk) {
 	return ((stk->top == stk->full));
 }
