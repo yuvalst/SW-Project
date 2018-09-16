197f742d7ae632d888c2865aed9d13858f6c03ce
Yuval Straussberg <yuvalstst@gmail.com>
2018-09-16 17:48:40 +0300

    Update Stack.h

--- Stack.h
+++ Stack.h
@@ -22,13 +22,20 @@ typedef struct stack {
    int full;
 } stack;
 
-
+/* Receives gameData and stack pointer and inisializes a new stack object*/
 void initStack(gameData * game, stack *stk);
+/* Receives stack, i and j for coordinates on the board and z for value and pushes them to the stack array */
 void push(stack *stk, int i, int j, int z);
+/* Receives stack and int pointer and updates cell with the values of the last cell on the stack like top, 
+but also removes the last cell from the stack*/
 void pop(stack *stk, int* cell);
+/* Receives stack and int pointer and updates cell with the values of the last cell on the stack like top*/
 void top(stack *stk, int* cell) ;
+/* Receives stack and returns 1 if the stack is empty else 0*/
 int isEmpty( stack *stk);
+/* Receives stack and returns 1 if the stack is full else 0*/
 int isFull( stack *stk);
+/* Receives stack and frees allocated memory*/
 void freeStack(stack *stk);
 
 #endif /* STACK_H_ */
